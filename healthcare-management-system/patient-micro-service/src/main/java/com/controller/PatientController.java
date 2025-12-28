0fea90b8179d4634dfad727df53623b747a76acf
ishan19-98 <choudharyishan38@gmail.com>
2025-12-28 11:25:13 +0530

    Pagination And Sorting In Patient Service

--- healthcare-management-system/patient-micro-service/src/main/java/com/controller/PatientController.java
+++ healthcare-management-system/patient-micro-service/src/main/java/com/controller/PatientController.java
@@ -4,12 +4,17 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.entity.Patient;
@@ -49,9 +54,16 @@ public class PatientController {
 	}
 	
 	@GetMapping(value = "",produces = MediaType.APPLICATION_JSON)
-	public Optional<List<Patient>> findAllPatient()
+	public Optional<Page<Patient>> findAllPatient(@RequestParam(defaultValue = "0") int page,
+			@RequestParam(defaultValue = "5") int size,
+			@RequestParam(defaultValue = "pid") String sortBy,
+			@RequestParam(defaultValue = "asc") String direction)
 	{
-		return patientService.findAllPatients();
+		
+		Sort sort = direction.equalsIgnoreCase("desc") ? Sort.by(sortBy).descending():Sort.by(sortBy).ascending();
+		
+		Pageable pageable = PageRequest.of(page, size, sort);
+		return patientService.findAllPatients(pageable);
 		
 	}
 
--- healthcare-management-system/patient-micro-service/src/main/java/com/service/PatientService.java
+++ healthcare-management-system/patient-micro-service/src/main/java/com/service/PatientService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.entity.Patient;
@@ -70,8 +72,8 @@ public class PatientService {
 		}
 	}
 
-	public Optional<List<Patient>> findAllPatients() {
-		return Optional.of(patientRepository.findAll());
+	public Optional<Page<Patient>> findAllPatients(Pageable pageable) {
+		return Optional.of(patientRepository.findAll(pageable));
 	}
 
 }
